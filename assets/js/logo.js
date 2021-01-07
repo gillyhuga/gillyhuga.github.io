95e46e7ded4e82866a30875cb9707f3432c8e433
gillyhuga <gilly.huga@yahoo.co.id>
2021-01-07 20:45:56 +0700

    Add logo

---
+++ assets/js/logo.js
@@ -0,0 +1,15 @@
+    $(document).ready(function() {
+        var text = $(".text");
+
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+
+        if(scroll >=300){
+            text.removeClass("hidden");
+            
+        }
+        else{
+            text.addClass("hidden")
+        }
+    });
+    });
