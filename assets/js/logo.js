a0b074991826ed0373780d8e6143b00054678f7f
gillyhuga <gilly.huga@yahoo.co.id>
2021-02-18 22:33:53 +0700

    add resume

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -1,16 +1,18 @@
 var scrolled=0;
     $(document).ready(function() {
         var text = $(".text");
+        //var footer = $(".footer__icon");
 
     $(window).scroll(function(){
         var scroll = $(window).scrollTop();
 
         if(scroll >=10){
             text.removeClass("hidden");
-            
+            //footer.removeClass("footertop");
         }
         else{
             text.addClass("hidden")
+            //footer.removeClass("footertop");
         }
     });
  });
